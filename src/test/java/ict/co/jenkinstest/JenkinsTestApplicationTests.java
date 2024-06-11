44f10a1fadcb44ac76dd7a51e9ab7bcb4680e9fa
mkw111 <rhauddn111@gmail.com>
2024-06-11 11:15:11 +0900

    first commit

---
+++ src/test/java/ict/co/jenkinstest/JenkinsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ict.co.jenkinstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
